9d2410e617e3f7d301ae65d6db911c0c747a19de
phil <philippspieck@yahoo.de>
2018-04-05 16:40:04 +0200

    test2

--- src/states/spielScreen.ts
+++ src/states/spielScreen.ts
@@ -13,7 +13,6 @@ export default class SpielScreen extends Phaser.State {
     public update(): void {
        // hier spaeter ueberpruefung ob tasten gedrueckt werden
        // oder alles was sich bewegen/veraendern soll
-
-        // TEST       
+       // TEST 2
       }
 }
